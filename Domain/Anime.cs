79c0b90dd67ba843446eeeb95d2cd26e97f13834
Andrej Zarkovski <zarke998@gmail.com>
2020-04-16 21:57:33 +0200

    Added Anime class to domain

---
+++ Domain/Anime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeTime.Core.Domain
+{
+    public class Anime
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string TitleAlt { get; set; }
+        public string Description { get; set; }
+        public string CoverUrl { get; set; }
+        public string CoverThumbUrl { get; set; }
+        public float Rating { get; set; }
+        public int ReleaseYear { get; set; }
+    }
+}
