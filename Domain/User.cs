c2e88f9bfd838786f2ad4228bfe0c6f66404a3f0
Andrej Zarkovski <zarke998@gmail.com>
2020-04-18 12:10:47 +0200

    Added User class to domain

---
+++ Domain/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeTime.Core.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+    }
+}
