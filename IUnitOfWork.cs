f55b92b93b5cd4bf2d99335e4a7d920183966f40
Andrej Zarkovski <zarke998@gmail.com>
2020-05-01 20:14:27 +0200

    Added repository pattern interfaces

---
+++ IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnimeTime.Core.Repositories.Interfaces;
+
+namespace AnimeTime.Core
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IAnimeRepository Animes { get; }
+        ICategoryRepository Categories { get; }
+        IYearSeasonRepository YearSeasons { get; }
+        IGenreRepository Genres { get; }
+
+        void Complete();
+    }
+}
---
+++ Repositories/Interfaces/IYearSeasonRepository.cs
@@ -0,0 +1,13 @@
+﻿using AnimeTime.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeTime.Core.Repositories.Interfaces
+{
+    public interface IYearSeasonRepository : IRepository<YearSeason>
+    {
+    }
+}
