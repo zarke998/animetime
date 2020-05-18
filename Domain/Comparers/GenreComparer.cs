d323aa676d9f4f7aa0c957be45076293011cbcb4
Andrej Zarkovski <zarke998@gmail.com>
2020-05-18 12:58:33 +0200

    Add GenreComparer
    Used for comparing genres by their name

---
+++ Domain/Comparers/GenreComparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeTime.Core.Domain.Comparers
+{
+    public class GenreComparer : IEqualityComparer<Genre>
+    {
+        public bool Equals(Genre x, Genre y)
+        {
+            if (y == null)
+                return false;
+
+            return x.Name == y.Name;
+        }
+
+        public int GetHashCode(Genre g)
+        {
+            return g.Name.GetHashCode();
+        }
+    }
+}
