8abff029c29002754932d8082c6a298997a7a3dc
Andrej Zarkovski <zarke998@gmail.com>
2020-05-20 11:39:44 +0200

    Add YearSeasonComparer
    Used for comparing year seasons by their name

---
+++ Domain/Comparers/YearSeasonComparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeTime.Core.Domain.Comparers
+{
+    public class YearSeasonComparer : IEqualityComparer<YearSeason>
+    {
+        public bool Equals(YearSeason x, YearSeason y)
+        {
+            if (y == null)
+                return false;
+
+            return x.Name == y.Name;
+        }
+
+        public int GetHashCode(YearSeason y)
+        {
+            return y.Name.GetHashCode();
+        }
+    }
+}
