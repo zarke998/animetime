e12375db31ac0aeba4e892f84e2430dfaecf0e00
Andrej Zarkovski <zarke998@gmail.com>
2020-05-20 11:59:23 +0200

    Add CategoryComparer
    Used for comparing categories by their name

---
+++ Domain/Comparers/CategoryComparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeTime.Core.Domain.Comparers
+{
+    public class CategoryComparer : IEqualityComparer<Category>
+    {
+        public bool Equals(Category x, Category y)
+        {
+            if (y == null)
+                return false;
+
+            return x.Name == y.Name;
+        }
+
+        public int GetHashCode(Category c)
+        {
+            return c.Name.GetHashCode();
+        }
+    }
+}
