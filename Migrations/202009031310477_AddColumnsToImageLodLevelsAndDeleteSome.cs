8393a2d6a72155103ddd1fd68742b9ecfe625a18
Andrej <zarke998@gmail.com>
2020-09-03 15:32:34 +0200

    Add more columns to ImageLodLevels, delete some
    Deleted column MaxSizeInKb since it is not needed, and added
    Quality, MaxWidthLandscape, MaxHeightPortrait columns.

---
+++ Migrations/202009031310477_AddColumnsToImageLodLevelsAndDeleteSome.cs
@@ -0,0 +1,24 @@
+﻿namespace AnimeTime.Persistence.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddColumnsToImageLodLevelsAndDeleteSome : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.ImageLodLevels", "MaxHeightPortrait", c => c.Int(nullable: false));
+            AddColumn("dbo.ImageLodLevels", "MaxWidthLandscape", c => c.Int(nullable: false));
+            AddColumn("dbo.ImageLodLevels", "Quality", c => c.Single(nullable: false));
+            DropColumn("dbo.ImageLodLevels", "MaxSizeInKb");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.ImageLodLevels", "MaxSizeInKb", c => c.Int(nullable: false));
+            DropColumn("dbo.ImageLodLevels", "Quality");
+            DropColumn("dbo.ImageLodLevels", "MaxWidthLandscape");
+            DropColumn("dbo.ImageLodLevels", "MaxHeightPortrait");
+        }
+    }
+}
