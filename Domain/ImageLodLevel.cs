13f9145dcf16df0342ad63bb2881e4b98f16f694
Andrej <zarke998@gmail.com>
2020-09-03 13:01:39 +0200

    Add lod levels
    Added enum with fixed lod levels, and changed Level property in
    ImageLodLevel to LodLevel enum type.

--- Domain/ImageLodLevel.cs
+++ Domain/ImageLodLevel.cs
@@ -9,7 +9,7 @@ namespace AnimeTime.Core.Domain
     public class ImageLodLevel
     {
         public int Id { get; set; }
-        public int Level { get; set; }
+        public LodLevel Level { get; set; }
         public string Name { get; set; }
         public int MaxSizeInKb { get; set; }
     }
---
+++ Domain/LodLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeTime.Core.Domain
+{
+    public enum LodLevel
+    {
+        Original = 1,
+        Big = 5,
+        Medium = 10,
+        Small = 15
+    }
+}
