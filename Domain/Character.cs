2ebe152c209e15686ccf43e721e565f98ebed463
Andrej <zarke998@gmail.com>
2020-09-08 12:21:23 +0200

    Add properties to Character, and remove some
    Added Description and SourceUrl properties to Character, and removed
    ImageUrl

--- Domain/Character.cs
+++ Domain/Character.cs
@@ -14,7 +14,8 @@ namespace AnimeTime.Core.Domain
         }
         public int Id { get; set; }
         public string Name { get; set; }
-        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+        public string SourceUrl { get; set; }
 
         public ICollection<Anime> Animes { get; set; }
     }
