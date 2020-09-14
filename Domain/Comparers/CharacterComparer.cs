9cd62387daba3c1f69f02dbce9f52589c61d2540
Andrej <zarke998@gmail.com>
2020-09-14 17:10:59 +0200

    Add Character comparer

---
+++ Domain/Comparers/CharacterComparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeTime.Core.Domain.Comparers
+{
+    public class CharacterComparer : IEqualityComparer<Character>
+    {
+        public bool Equals(Character x, Character y)
+        {
+            if (y == null)
+                return false;
+
+            return x.SourceUrl == y.SourceUrl;
+        }
+
+        public int GetHashCode(Character c)
+        {
+            return c.SourceUrl.GetHashCode();
+        }
+    }
+}
