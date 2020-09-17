2b373172c04782c35e367cc2ccb0a41da1d148ae
Andrej <zarke998@gmail.com>
2020-09-17 11:52:44 +0200

    Move ILogger to AnimeTime.Core library

--- Utilities/ConsoleLogger.cs
+++ Utilities/ConsoleLogger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AnimeTimeDbUpdater.Core;
+using AnimeTime.Core.Interfaces.Utilities;
 
 namespace AnimeTimeDbUpdater.Utilities
 {
--- Utilities/FileLogger.cs
+++ Utilities/FileLogger.cs
@@ -1,4 +1,4 @@
-﻿using AnimeTimeDbUpdater.Core;
+﻿using AnimeTime.Core.Interfaces.Utilities;
 using System;
 using System.Collections.Generic;
 using System.IO;
