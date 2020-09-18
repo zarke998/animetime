cee0a30ac9c4810bb08f5250b508585f8f9acd47
Andrej <zarke998@gmail.com>
2020-09-18 18:00:37 +0200

    Add image downloader

---
+++ Imaging/ImageDownloader.cs
@@ -0,0 +1,30 @@
+﻿using AnimeTime.Core.Utilities.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeTime.Utilities.Imaging
+{
+    public class ImageDownloader : IImageDownloader
+    {
+        public Image Download(string imageUrl)
+        {
+            using (WebClient client = new WebClient())
+            {
+                byte[] imgData = client.DownloadData(imageUrl);
+
+                using(var memoryStream = new MemoryStream(imgData))
+                {
+                    var image = Image.FromStream(memoryStream);
+
+                    return image;
+                }
+            }
+        }
+    }
+}
