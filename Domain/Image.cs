29819e7ff7a6953e6cc4353cdd9bc6c2ba01ba2f
Andrej <zarke998@gmail.com>
2020-10-09 15:59:38 +0200

    Add orientation and image type id props in Image

--- Domain/Image.cs
+++ Domain/Image.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AnimeTime.Core.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,11 @@ namespace AnimeTime.Core.Domain
 
         public int Id { get; set; }
         public string Url { get; set; }
+
+        public ImageTypeId ImageType_Id { get; set; }
         public ImageType ImageType { get; set; }
+
+        public ImageOrientationId Orientation_Id { get; set; }
         public ImageOrientation Orientation { get; set; }
 
         public ICollection<Thumbnail> Thumbnails { get; set; }
