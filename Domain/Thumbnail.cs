fb9a444c31b1e893e66e0a963df52e12983a4f90
Andrej <zarke998@gmail.com>
2020-10-09 21:28:43 +0200

    Add lod level id prop in Thumbnail

--- Domain/Thumbnail.cs
+++ Domain/Thumbnail.cs
@@ -11,7 +11,9 @@ namespace AnimeTime.Core.Domain
         public int Id { get; set; }
         public string Url { get; set; }
 
+        public int ImageLodLevel_Id { get; set; }
         public ImageLodLevel ImageLodLevel { get; set; }
+
         public Image Image { get; set; }
     }
 }
