5b235a5972d5d7e42ab1fd35e9641d5485fbec98
Andrej <zarke998@gmail.com>
2020-10-14 17:01:12 +0200

    Add EntityInsertException

---
+++ Exceptions/EntityInsertException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeTime.Core.Exceptions
+{
+    public class EntityInsertException : Exception
+    {
+        public EntityInsertException(string message) : base(message)
+        {
+        }
+
+        public EntityInsertException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
