7699007109cb9431a6a69f4bc584c68d110c4202
Andrej <zarke998@gmail.com>
2020-10-16 12:40:20 +0200

    Remove CoverUrl and CoverThumbUrl cols from Anime

---
+++ Migrations/202010161039141_RemoveCoverUrlAndCoverThumbUrlColsFromAnime.cs
@@ -0,0 +1,20 @@
+﻿namespace AnimeTime.Persistence.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RemoveCoverUrlAndCoverThumbUrlColsFromAnime : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Animes", "CoverUrl");
+            DropColumn("dbo.Animes", "CoverThumbUrl");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Animes", "CoverThumbUrl", c => c.String());
+            AddColumn("dbo.Animes", "CoverUrl", c => c.String());
+        }
+    }
+}
