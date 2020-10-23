7cc8e45a29777ea2ae37969f6cf1273c7a3e6fda
Andrej <zarke998@gmail.com>
2020-10-23 11:05:52 +0200

    [Api] Self-host api via OWIN

---
+++ windows/animetime-web-api/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace AnimeTime.WebAPI
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var serverAddress = "http://localhost:9000/";
+
+            using (var app = WebApp.Start<Startup>(serverAddress))
+            {
+                var client = new HttpClient();
+
+                var response = client.GetAsync(serverAddress + "api/").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ windows/animetime-web-api/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace AnimeTime.WebAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var configuration = new HttpConfiguration();
+
+            configuration.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+
+            appBuilder.UseWebApi(configuration);
+        }
+    }
+}
