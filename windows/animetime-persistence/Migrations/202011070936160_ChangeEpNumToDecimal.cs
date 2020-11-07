2f01a84157f9bbad1b67144fd45e13f0186199ab
Andrej <zarke998@gmail.com>
2020-11-07 11:07:48 +0100

    [Pers] Change EpNum type to decimal

---
+++ windows/animetime-persistence/Migrations/202011070936160_ChangeEpNumToDecimal.cs
@@ -0,0 +1,18 @@
+﻿namespace AnimeTime.Persistence.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangeEpNumToDecimal : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Episodes", "EpNum", c => c.Decimal(nullable: false, precision: 5, scale: 1));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Episodes", "EpNum", c => c.Int(nullable: false));
+        }
+    }
+}
