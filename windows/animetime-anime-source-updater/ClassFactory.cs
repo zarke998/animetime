6510b797843de01e0def89c94f45cc66e858a310
Andrej <zarke998@gmail.com>
2020-11-14 12:51:02 +0100

    [Asu] Update anime sources

---
+++ windows/animetime-anime-source-updater/ClassFactory.cs
@@ -0,0 +1,13 @@
+﻿using AnimeTime.Core;
+using AnimeTime.Persistence;
+using AnimeTime.Utilities;
+using System;
+
+namespace AnimeTimeAnimeSourceUpdater
+{
+    internal class ClassFactory
+    {
+        public static IUnitOfWork CreateUnitOfWork() => new UnitOfWork(new AnimeTimeDbContext());
+        public static ICrawlDelayer CreateCrawlDelayer(double crawlWait = 1.0, double crawlWaitOffset = 0.2) => new CrawlDelayer(crawlWait, crawlWaitOffset);
+    }
+}
