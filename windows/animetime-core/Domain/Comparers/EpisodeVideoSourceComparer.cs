97ec1f10c4ea86836a43360359e91ccac24f61d4
Andrej <zarke998@gmail.com>
2020-11-23 17:13:19 +0100

    [Core] Add EpisodeVideoSourceComparer

---
+++ windows/animetime-core/Domain/Comparers/EpisodeVideoSourceComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeTime.Core.Domain.Comparers
+{
+    public class EpisodeVideoSourceComparer : IEqualityComparer<EpisodeVideoSource>
+    {
+        public bool Equals(EpisodeVideoSource x, EpisodeVideoSource y)
+        {
+            if (y == null || x.Url == null || y.Url == null) return false;
+
+            return x.Url == y.Url;
+        }
+
+        public int GetHashCode(EpisodeVideoSource obj)
+        {
+            if (obj.Url == null || obj.Url == null) return 0;
+
+            return obj.Url.GetHashCode();
+        }
+    }
+}
