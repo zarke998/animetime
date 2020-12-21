4c87c36333a5758ade938ed2744f45836ed2f17e
Andrej <zarke998@gmail.com>
2020-12-21 17:40:06 +0100

    Testing StreamtapePlayer

--- android/animetime-app/app/src/main/java/com/example/animetime/MainActivity.java
+++ android/animetime-app/app/src/main/java/com/example/animetime/MainActivity.java
@@ -15,6 +15,7 @@ import android.widget.RelativeLayout;
 
 import com.example.animetime.utils.htmlembedplayers.JWPlayer;
 import com.example.animetime.utils.htmlembedplayers.MixdropPlayer;
+import com.example.animetime.utils.htmlembedplayers.StreamtapePlayer;
 import com.example.animetime.widgets.AnimeTimeWebView;
 
 import org.adblockplus.libadblockplus.android.webview.AdblockWebView;
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     private JWPlayer mJWPlayer;
     private MixdropPlayer mMixdropPlayer;
+    private StreamtapePlayer mStreamtapePlayer;
 
     private AnimeTimeWebView mWebView;
 
@@ -36,13 +38,15 @@ public class MainActivity extends AppCompatActivity {
 
         mJWPlayer = new JWPlayer(mWebView);
         mMixdropPlayer = new MixdropPlayer(mWebView);
+        mStreamtapePlayer = new StreamtapePlayer(mWebView);
 
 //        mWebView.loadUrl("https://gogo-stream.com/streaming.php?id=MTE3MTU=&title=Bleach+Episode+366");
-        mWebView.loadUrl("https://mixdrop.co/e/dqkj98rqbez9mj");
+//        mWebView.loadUrl("https://mixdrop.co/e/dqkj98rqbez9mj");
+        mWebView.loadUrl("https://streamta.pe/e/bQbAopJKlBsPM78/bleach-episode-366.mp4");
     }
 
     public void testBtnClick(View v){
-        mMixdropPlayer.hidePlayerControlsAsync(() -> {
-            Log.d(TAG, "Controls hidden.");
+        mStreamtapePlayer.playAsync(() -> {
+            Log.d(TAG, "Playing.");
         });
     }}
