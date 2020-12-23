0e3f2c9ccb040411848ce955929e73598c47795e
Andrej <zarke998@gmail.com>
2020-12-23 13:27:16 +0100

    Test VideoJsPlayer for Mp4Upload

--- android/animetime-app/app/src/main/java/com/example/animetime/MainActivity.java
+++ android/animetime-app/app/src/main/java/com/example/animetime/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,9 +16,11 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 
 
+import com.example.animetime.utils.htmlembedplayers.HtmlEmbedPlayerBase;
 import com.example.animetime.utils.htmlembedplayers.JWPlayer;
 import com.example.animetime.utils.htmlembedplayers.MixdropPlayer;
 import com.example.animetime.utils.htmlembedplayers.StreamtapePlayer;
+import com.example.animetime.utils.htmlembedplayers.VideoJsPlayer;
 import com.example.animetime.widgets.AnimeTimeWebView;
 
 import org.adblockplus.libadblockplus.android.webview.AdblockWebView;
@@ -28,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private JWPlayer mJWPlayer;
     private MixdropPlayer mMixdropPlayer;
     private StreamtapePlayer mStreamtapePlayer;
+    private VideoJsPlayer mMp4UploadPlayer;
 
     private AnimeTimeWebView mWebView;
     private Button mTestBtn;
@@ -46,83 +50,87 @@ public class MainActivity extends AppCompatActivity {
         mJWPlayer = new JWPlayer(mWebView);
         mMixdropPlayer = new MixdropPlayer(mWebView);
         mStreamtapePlayer = new StreamtapePlayer(mWebView);
+        mMp4UploadPlayer = new VideoJsPlayer(mWebView);
 
 //        mWebView.loadUrl("https://gogo-stream.com/streaming.php?id=MTE3MTU=&title=Bleach+Episode+366");
 //        mWebView.loadUrl("https://mixdrop.co/e/dqkj98rqbez9mj");
-        mWebView.loadUrl("https://streamta.pe/e/bQbAopJKlBsPM78/bleach-episode-366.mp4");
+//        mWebView.loadUrl("https://streamta.pe/e/bQbAopJKlBsPM78/bleach-episode-366.mp4");
+        mWebView.loadUrl("https://www.mp4upload.com/embed-hzl1fw4wkedt.html");
     }
+
 
     @SuppressLint("LogNotTimber")
     public void testBtnClick(View v){
+        HtmlEmbedPlayerBase player = mMp4UploadPlayer;
 
         switch (mTestCaseIndex) {
             case -1:
                 Log.d(TAG, "Setup test.");
-                mStreamtapePlayer.setup(() -> {
+                player.setupAsync(() -> {
                     Log.d(TAG, "Setup done.");
                 });
                 break;
             case 0:
                 Log.d(TAG, "Play test 1.");
-                mStreamtapePlayer.playAsync(() -> {
+                player.playAsync(() -> {
                     Log.d(TAG, "Playing.");
                 });
                 break;
             case 1:
                 Log.d(TAG, "Pause test.");
-                mStreamtapePlayer.pauseAsync(() -> {
+                player.pauseAsync(() -> {
                     Log.d(TAG, "Paused.");
                 });
                 break;
             case 2:
                 Log.d(TAG, "Play test 2.");
-                mStreamtapePlayer.playAsync(() -> {
+                player.playAsync(() -> {
                     Log.d(TAG, "Playing.");
                 });
                 break;
             case 3:
                 Log.d(TAG, "Seek test.");
-                mStreamtapePlayer.seekAsync(900, () -> {
+                player.seekAsync(900, () -> {
                     Log.d(TAG, "Seeked.");
                 });
                 break;
             case 4:
                 Log.d(TAG, "Duration test.");
-                mStreamtapePlayer.getVideoDurationAsync(duration -> {
+                player.getVideoDurationAsync(duration -> {
                     Log.d(TAG, "Video duration:" + duration);
                 });
                 break;
             case 5:
                 Log.d(TAG, "Position test");
-                mStreamtapePlayer.getVideoPositionAsync(position -> {
+                player.getVideoPositionAsync(position -> {
                     Log.d(TAG, "Position: " + position);
                 });
                 break;
             case 6:
                 Log.d(TAG, "Get volume test.");
-                mStreamtapePlayer.getVolumeAsync(volume -> {
+                player.getVolumeAsync(volume -> {
                     Log.d(TAG, "Volume: " + volume);
                 });
                 break;
             case 7:
                 Log.d(TAG, "Set volume test.");
-                mStreamtapePlayer.setVolumeAsync(50, () -> {
+                player.setVolumeAsync(50, () -> {
                     Log.d(TAG, "Volume set.");
                 });
                 break;
             case 8:
                 Log.d(TAG, "Fullscreen test.");
-                if (mStreamtapePlayer.isFullscreenProtected()) {
+                if (player.isFullscreenProtected()) {
                     Log.d(TAG, "Fullscreen protected.");
                 } else {
-                    mStreamtapePlayer.setFullscreenAsync(true, () -> {
+                    player.setFullscreenAsync(true, () -> {
                         Log.d(TAG, "Fullscreened.");
                     });
                 }
                 break;
             case 9:
                 Log.d(TAG, "Hide controls test.");
-                mStreamtapePlayer.hidePlayerControlsAsync(() -> {
+                player.hidePlayerControlsAsync(() -> {
                     Log.d(TAG, "Controls hidden.");
                 });
                 break;
--- android/animetime-app/app/src/main/java/com/example/animetime/widgets/AnimeTimeWebView.java
+++ android/animetime-app/app/src/main/java/com/example/animetime/widgets/AnimeTimeWebView.java
@@ -41,6 +41,7 @@ public class AnimeTimeWebView extends AdblockWebView {
     }
 
     private void configureWebView(){
+        this.setWebContentsDebuggingEnabled(true);
         this.getSettings().setJavaScriptEnabled(true);
         this.getSettings().setUserAgentString("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
 
