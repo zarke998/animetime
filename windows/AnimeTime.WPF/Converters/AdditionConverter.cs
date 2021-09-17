4da6871a52617021b6c92f6a49070618d62ce182
Andrej <zarke998@gmail.com>
2021-09-17 14:25:37 +0200

    feat(wpf): [WIP] add search results

---
+++ windows/AnimeTime.WPF/Converters/AdditionConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace AnimeTime.WPF.Converters
+{
+    public class AdditionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var doubleVal = (double)value;
+            var doubleParam = Double.Parse(parameter.ToString());
+
+            return doubleVal + doubleParam;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ windows/AnimeTime.WPF/Views/ExtendedControls/SearchBox.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimeTime.WPF.Views.ExtendedControls
+{
+    /// <summary>
+    /// Interaction logic for Search.xaml
+    /// </summary>
+    public partial class SearchBox : TextBox
+    {
+        public SearchBox()
+        {
+            InitializeComponent();
+            this.GotFocus += Search_GotFocus;
+        }
+
+        private void Search_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var tb = e.OriginalSource as TextBox;
+            if (tb == this) e.Handled = true;
+        }
+    }
+}
