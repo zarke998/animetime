292525a2b514aa0d5ee16ffff03ddbea01c8ada9
Andrej <zarke998@gmail.com>
2021-11-10 02:20:57 +0100

    fix(wpf): fixed main window border clipping

---
+++ windows/AnimeTime.WPF/Converters/BorderToRectConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace AnimeTime.WPF.Converters
+{
+    public class WidthHeightToRectConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new Rect(new Size((double)values[0], (double)values[1]));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
