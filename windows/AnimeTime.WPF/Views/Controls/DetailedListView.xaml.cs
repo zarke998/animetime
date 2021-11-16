198019d84d132c0be9b48d438b884f6534407d45
Andrej <zarke998@gmail.com>
2021-11-17 00:24:42 +0100

    feat(wpf): add detailed list view

---
+++ windows/AnimeTime.WPF/Views/Controls/DetailedListView.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimeTime.WPF.Views.Controls
+{
+    /// <summary>
+    /// Interaction logic for DetailedListView.xaml
+    /// </summary>
+    public partial class DetailedListView : UserControl
+    {
+        #region Dependency Properties
+
+
+        public ObservableCollection<DetailedItem> Items
+        {
+            get { return (ObservableCollection<DetailedItem>)GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Items.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemsProperty =
+            DependencyProperty.Register("Items", typeof(ObservableCollection<DetailedItem>), typeof(DetailedListView), new PropertyMetadata(new ObservableCollection<DetailedItem>()));
+
+
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(DetailedListView), new PropertyMetadata("Title"));
+
+
+        #endregion
+
+        public DetailedListView()
+        {
+            InitializeComponent();
+            Items.Add(new DetailedItem()
+            {
+                Title = "Test",
+                Rating = 9.1f,
+                ReleaseYear = 1999,
+                ContentType = "TV",
+                Description = "This is a description..."
+            });
+            Items.Add(new DetailedItem()
+            {
+                Title = "Test",
+                Rating = 9.1f,
+                ReleaseYear = 1999,
+                ContentType = "TV",
+                Description = "This is a description...This is a description...This is a description...This is a description...This is a description...This is a description...This is a description...This is a description...This is a description..."
+            });
+        }
+    }
+
+    public class DetailedItem
+    {
+        public string Title { get; set; }
+        public float Rating { get; set; }
+        public int ReleaseYear { get; set; }
+        public string ContentType { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ windows/AnimeTime.WPF/Views/Controls/TextRated.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimeTime.WPF.Views.Controls
+{
+    /// <summary>
+    /// Interaction logic for TextRated.xaml
+    /// </summary>
+    public partial class TextRated : UserControl
+    {
+        #region Dependency Properties
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(TextRated), new PropertyMetadata(String.Empty));
+
+
+
+        public double Rating
+        {
+            get { return (double)GetValue(RatingProperty); }
+            set { SetValue(RatingProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Rating.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RatingProperty =
+            DependencyProperty.Register("Rating", typeof(double), typeof(TextRated), new PropertyMetadata(0.0));
+
+
+        #endregion
+
+        public TextRated()
+        {
+            InitializeComponent();
+        }
+    }
+}
