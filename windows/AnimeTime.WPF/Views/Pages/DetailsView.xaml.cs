90eed49710b558675043b3e827e9c1186007f7e1
Andrej <zarke998@gmail.com>
2021-11-30 18:20:38 +0100

    feat(wpf): animate wallpaper in DetailsView

--- windows/AnimeTime.WPF/Views/Pages/DetailsView.xaml.cs
+++ windows/AnimeTime.WPF/Views/Pages/DetailsView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using AnimeTime.WPF.Views.ExtendedControls;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,11 +32,47 @@ namespace AnimeTime.WPF.Views.Pages
         // Using a DependencyProperty as the backing store for VerticalContentOffset.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty VerticalContentOffsetProperty =
             DependencyProperty.Register("VerticalContentOffset", typeof(double), typeof(DetailsView), new PropertyMetadata(0.0));
+
+
+        public bool WallpaperPassedViewport
+        {
+            get { return (bool)GetValue(WallpaperPassedViewportProperty); }
+            set { SetValue(WallpaperPassedViewportProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for WallpaperPassedViewport.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty WallpaperPassedViewportProperty =
+            DependencyProperty.Register("WallpaperPassedViewport", typeof(bool), typeof(DetailsView), new PropertyMetadata(false));
         #endregion
+
+        private StackPanel ContentContainer;
+        private DefaultScrollViewer DefaultScrollViewer;
 
         public DetailsView()
         {
             InitializeComponent();
+            this.Loaded += DetailsView_Loaded;
+        }
+
+        private void DetailsView_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitializeTemplateVariables();
+            DefaultScrollViewer.ScrollChanged += DefaultScrollViewer_ScrollChanged;
+        }
+
+        private void InitializeTemplateVariables()
+        {
+            ContentContainer = this.Template.FindName("ContentContainer", this) as StackPanel;
+            DefaultScrollViewer = this.Template.FindName("DefaultScrollViewer", this) as DefaultScrollViewer;
+        }
+
+        private void DefaultScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            var point = ContentContainer.TranslatePoint(new Point(), DefaultScrollViewer);
+            if (point.Y <= 0)
+                WallpaperPassedViewport = true;
+            else
+                WallpaperPassedViewport = false;
         }
     }
 }
