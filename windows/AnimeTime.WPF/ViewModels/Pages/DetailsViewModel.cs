740e3c3f0ac0b83ef9ba8f300c396631e518eb13
Andrej <zarke998@gmail.com>
2021-12-01 18:23:11 +0100

    feat(wpf): add genres section in DetailsView

--- windows/AnimeTime.WPF/ViewModels/Pages/DetailsViewModel.cs
+++ windows/AnimeTime.WPF/ViewModels/Pages/DetailsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using AnimeTime.WPF.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,60 @@ namespace AnimeTime.WPF.ViewModels.Pages
 {
     public class DetailsViewModel : ViewModelBase
     {
-        private double _verticalContentOffset;
+        public ObservableCollection<Genre> Genres { get; set; }
 
-        public double VerticalContentOffset
+        public DetailsViewModel()
         {
-            get { return _verticalContentOffset; }
-            set { _verticalContentOffset = value; OnPropertyChanged(); }
+            Genres = new ObservableCollection<Genre>()
+            { 
+                new Genre()
+                { 
+                    Name = "Action" 
+                },
+                new Genre() 
+                { 
+                    Name = "Adventure" 
+                },
+                new Genre()
+                {
+                    Name = "Adventure"
+                }
+                ,
+                new Genre()
+                {
+                    Name = "Adventure"
+                }
+                ,
+                new Genre()
+                {
+                    Name = "Adventure"
+                }
+                ,
+                new Genre()
+                {
+                    Name = "Adventure"
+                }
+                ,
+                new Genre()
+                {
+                    Name = "Adventure"
+                }
+                ,
+                new Genre()
+                {
+                    Name = "Adventure"
+                }
+                ,
+                new Genre()
+                {
+                    Name = "Adventure"
+                }
+            };
         }
+    }
 
+    public class Genre
+    {
+        public string Name { get; set; }
     }
 }
