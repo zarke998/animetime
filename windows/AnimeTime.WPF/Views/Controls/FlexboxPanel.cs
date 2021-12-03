68f353afb8d05d8b6a46c0e58465db8292be518f
Andrej <zarke998@gmail.com>
2021-12-04 00:11:28 +0100

    feat(wpf): add SpaceEven to FlexboxPanel

--- windows/AnimeTime.WPF/Views/Controls/FlexboxPanel.cs
+++ windows/AnimeTime.WPF/Views/Controls/FlexboxPanel.cs
@@ -11,6 +11,35 @@ namespace AnimeTime.WPF.Views.Controls
 {
     public class FlexboxPanel : Panel
     {
+        #region Dependency Properties
+
+
+
+        public double Spacing
+        {
+            get { return (double)GetValue(SpacingProperty); }
+            set { SetValue(SpacingProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Spacing.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SpacingProperty =
+            DependencyProperty.Register("Spacing", typeof(double), typeof(FlexboxPanel), new PropertyMetadata(0.0));
+
+
+
+        public JustifyOptions Justify
+        {
+            get { return (JustifyOptions)GetValue(JustifyProperty); }
+            set { SetValue(JustifyProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Justify.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty JustifyProperty =
+            DependencyProperty.Register("Justify", typeof(JustifyOptions), typeof(FlexboxPanel), new PropertyMetadata(JustifyOptions.SpaceBetween));
+
+
+        #endregion
+
         protected override Size MeasureOverride(Size availableSize)
         {
             var totalSize = new Size(0, 0);
@@ -29,6 +58,43 @@ namespace AnimeTime.WPF.Views.Controls
             return totalSize;
         }
         protected override Size ArrangeOverride(Size finalSize)
+        {
+            switch (Justify)
+            {
+                case JustifyOptions.SpaceBetween:
+                    return ArrangeSpaceBetween(finalSize);
+                case JustifyOptions.SpaceEven:
+                    return ArrangeSpaceEven(finalSize);
+            }
+            return new Size();
+        }
+
+        private Size ArrangeSpaceEven(Size finalSize)
+        {
+            double x = 0.0;
+            double y = 0.0;
+
+            var itemsPerRow = GetMaxItemsPerRow(finalSize.Width);
+            var fullItemWidth = InternalChildren[0].DesiredSize.Width + Spacing;            
+            var childInRow = 0;
+            var extraSpacing = (finalSize.Width - ((itemsPerRow - 1) * fullItemWidth + InternalChildren[0].DesiredSize.Width)) / (itemsPerRow - 1);
+            foreach (var child in InternalChildren.Cast<UIElement>())
+            {
+                childInRow++;
+                if(childInRow > itemsPerRow)
+                {
+                    childInRow = 1;
+                    y += child.DesiredSize.Height;
+                    x = 0;
+                }
+
+                child.Arrange(new Rect(new Point(x, y), child.DesiredSize));
+                x += child.DesiredSize.Width + Spacing + extraSpacing;
+            }
+
+            return finalSize;
+        }
+        private Size ArrangeSpaceBetween(Size finalSize)
         {
             double x = 0.0;
             double y = 0.0;
@@ -78,5 +144,17 @@ namespace AnimeTime.WPF.Views.Controls
 
             return rows;
         }
+        private int GetMaxItemsPerRow(double availableWidth)
+        {
+            if (InternalChildren.Count == 0) return 0;
+
+            return Convert.ToInt32((availableWidth + Spacing) / (InternalChildren[0].DesiredSize.Width + Spacing));
+        }
+    }
+
+    public enum JustifyOptions
+    {
+        SpaceBetween,
+        SpaceEven
     }
 }
