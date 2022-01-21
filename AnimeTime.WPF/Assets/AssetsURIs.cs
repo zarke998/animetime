eb48e35eb683aba45b1ad49061127878ffc02145
Andrej <zarke998@gmail.com>
2022-01-21 10:51:42 +0100

    feat(wpf): add Prequel section to DetailsView

---
+++ AnimeTime.WPF/Assets/AssetsURIs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeTime.WPF.Assets
+{
+    public static class AssetsURIs
+    {
+        public static string DefaultImage { get; } = "/Assets/Images/default-image.jpg";
+    }
+}
