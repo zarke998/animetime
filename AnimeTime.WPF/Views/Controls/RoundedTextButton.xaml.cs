2d96fa35c1da0f201073ecdbb68b2e8aae1c451c
Andrej <zarke998@gmail.com>
2022-01-21 12:26:09 +0100

    refactor(wpf): [WIP] extract Genres section to control

---
+++ AnimeTime.WPF/Views/Controls/RoundedTextButton.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimeTime.WPF.Views.Controls
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class RoundedTextButton : Button
+    {
+        #region Depdenency Properties
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(RoundedTextButton), new PropertyMetadata(String.Empty));
+
+
+        #endregion
+
+        public RoundedTextButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
