128541f88c7c39d2b0ca799eb4dac1925cf39b7b
Andrej <zarke998@gmail.com>
2022-03-04 14:16:59 +0100

    feat(wpf): add VLC player to PlayerWindow

--- AnimeTime.WPF/Views/PlayerWindow.xaml.cs
+++ AnimeTime.WPF/Views/PlayerWindow.xaml.cs
@@ -9,9 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using LibVLCSharp.Shared;
 
 namespace AnimeTime.WPF.Views
 {
@@ -20,9 +18,38 @@ namespace AnimeTime.WPF.Views
     /// </summary>
     public partial class PlayerWindow : WindowBase
     {
+        private LibVLC _libVLC;
         public PlayerWindow()
         {
             InitializeComponent();
+            InitializeVLC();
+            this.Loaded += PlayerWindow_Loaded;
+            this.Closed += PlayerWindow_Closed;
+        }
+
+        private void InitializeVLC()
+        {
+            LibVLCSharp.Shared.Core.Initialize();
+            _libVLC = new LibVLC();
+        }
+        private void ShutdownVLC()
+        {
+            VlcVideoView.MediaPlayer.Stop();
+            VlcVideoView.MediaPlayer.Dispose();
+            _libVLC.Dispose();
+        }
+
+        private void PlayerWindow_Closed(object sender, EventArgs e)
+        {
+            ShutdownVLC();
+        }
+
+        private void PlayerWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var mediaPlayer = new MediaPlayer(_libVLC);
+            VlcVideoView.MediaPlayer = mediaPlayer;
+
+            mediaPlayer.Play(new Media(_libVLC, new Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")));
         }
     }
 }
