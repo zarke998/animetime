a6d440c780bd6fb2edb753466a72c884cfe17b3e
Andrej <zarke998@gmail.com>
2022-04-08 15:38:48 +0200

    refactor(service): change how episode video sources refresh

--- AnimeTime.Services/ModelServices/VideoSourceService.cs
+++ AnimeTime.Services/ModelServices/VideoSourceService.cs
@@ -14,7 +14,6 @@ namespace AnimeTime.Services.ModelServices
 {
     public class VideoSourceService : IVideoSourceService
     {
-        private const int VIDEO_SOURCE_REFRESH_INTERVAL = 24;
         private readonly IUnitOfWork _unitOfWork;
         private readonly IWebsiteProcessorFactory _websiteProcessorFactory;
         private readonly IMapper _mapper;
@@ -30,16 +29,24 @@ namespace AnimeTime.Services.ModelServices
             var episode = _unitOfWork.Episodes.GetWithVideoSources(episodeId);
 
             var episodeMetadata = _unitOfWork.EpisodeMetadatas.Get(episodeId);
-            if(episodeMetadata != null &&
-                episodeMetadata.VideoSourcesLastUpdate.HasValue &&
-                (DateTime.UtcNow - episodeMetadata.VideoSourcesLastUpdate.Value).TotalHours > VIDEO_SOURCE_REFRESH_INTERVAL)
+            if (episodeMetadata != null && episodeMetadata.HasWorkingVideoSources)
             {
-                
                 return _mapper.Map<IEnumerable<VideoSourceDTO>>(episode.Sources.SelectMany(x => x.VideoSources));
             }
 
             _unitOfWork.EpisodeVideoSources.RemoveRange(episode.Sources.SelectMany(x => x.VideoSources));
 
+            var newVideoSources = FetchNewVideoSources(episode);
+
+            _unitOfWork.EpisodeVideoSources.AddRange(newVideoSources);
+            SetHasWorkingVideoSources(episodeMetadata, episodeId);
+            _unitOfWork.Complete();
+
+            return _mapper.Map<IEnumerable<VideoSourceDTO>>(newVideoSources);
+        }
+
+        private List<EpisodeVideoSource> FetchNewVideoSources(Episode episode)
+        {
             var newVideoSources = new List<EpisodeVideoSource>();
             foreach (var episodeSource in episode.Sources)
             {
@@ -47,21 +54,16 @@ namespace AnimeTime.Services.ModelServices
                 var processor = _websiteProcessorFactory.CreateWebsiteProcessor(website.Name, website.Url, website.QuerySuffix);
                 var videoSources = processor.GetVideoSourcesAsync(episodeSource.Url).Result;
 
-                newVideoSources.AddRange(videoSources.Select(videoSource => new EpisodeVideoSource() 
-                { 
-                    EpisodeSource_Id = episodeSource.Id, 
-                    Url = videoSource 
+                newVideoSources.AddRange(videoSources.Select(videoSource => new EpisodeVideoSource()
+                {
+                    EpisodeSource_Id = episodeSource.Id,
+                    Url = videoSource
                 }));
             }
 
-            _unitOfWork.EpisodeVideoSources.AddRange(newVideoSources);
-            UpdateVideoSourcesLastUpdate(episodeMetadata, episodeId);
-            _unitOfWork.Complete();
-
-            return _mapper.Map<IEnumerable<VideoSourceDTO>>(newVideoSources);
+            return newVideoSources;
         }
-
-        private void UpdateVideoSourcesLastUpdate(EpisodeMetadata episodeMetadata, int episodeId)
+        private void SetHasWorkingVideoSources(EpisodeMetadata episodeMetadata, int episodeId)
         {
             if (episodeMetadata == null)
             {
@@ -69,7 +71,7 @@ namespace AnimeTime.Services.ModelServices
                 _unitOfWork.EpisodeMetadatas.Add(episodeMetadata);
             }
 
-            episodeMetadata.VideoSourcesLastUpdate = DateTime.UtcNow;
+            episodeMetadata.HasWorkingVideoSources = true;
         }
     }
 }
