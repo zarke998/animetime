5ca4f2b52c10db2468cd41c186542ba29482dc7e
Andrej <zarke998@gmail.com>
2022-08-05 02:04:35 +0200

    fix(pers): fix AnimeSource Url col not nullable

---
+++ AnimeTime.Persistence/Migrations/202208042315058_SetAnimeSourceUrlToNonNullable.cs
@@ -0,0 +1,18 @@
+﻿namespace AnimeTime.Persistence.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SetAnimeSourceUrlToNonNullable : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.AnimeSources", "Url", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.AnimeSources", "Url", c => c.String());
+        }
+    }
+}
---
+++ AnimeTime.Persistence/Migrations/202208042316567_SetAnimeSourceUrlToNullable.cs
@@ -0,0 +1,18 @@
+﻿namespace AnimeTime.Persistence.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SetAnimeSourceUrlToNullable : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.AnimeSources", "Url", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.AnimeSources", "Url", c => c.String(nullable: false));
+        }
+    }
+}
